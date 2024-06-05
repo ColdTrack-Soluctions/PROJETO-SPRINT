742ea8606bd288ad7bd2d767097abbb38e88f81e
FilipeGuerreiro <filipe.guerreiro@sptech.school>
2024-06-04 22:50:09 -0300

    subindo atualizacoes na dash e nos cards

--- SPRINT 2/Site_Institucional/public/js/alerta.js
+++ SPRINT 2/Site_Institucional/public/js/alerta.js
@@ -11,9 +11,13 @@ function obterdados(a, b, c) {
                 resposta.json().then(resposta => {
 
                     // console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+                    if(resposta[0].Temperatura > 10){
+                        alerta(resposta[0].Temperatura, resposta[0].idRefrigerador, resposta[0].Horario)
+                    }
+
                     const element = document.getElementById(`temp_refrigerador_${parametros[0]}`);
                     if (element) {
-                        element.innerHTML = `${resposta[0].Temperatura} -°C`;
+                        element.innerHTML = `<span class="temperatura" id="temperatura">${resposta[0].Temperatura}</span> °C`;
                     } else {
                         console.error(`Elemento com ID temp_refrigerador_${parametros[0]} não encontrado.`);
                     }
@@ -68,107 +72,22 @@ function obterdadosportas(parametros) {
 
 }
 
-function alertar(resposta, idAquario) {
-    var temp = resposta[0].temperatura;
+function alerta(Temperatura, Refrigerador, Horario) {
 
-    var grauDeAviso = '';
+    
+    alert(`AVISO!, O REFRIGERADOR DE ID ${Refrigerador} ESTA COM A TEMPERATURA DE ${Temperatura} aos ${Horario}.`)
+    
 
-    var limites = {
-        muito_quente: 23,
-        quente: 22,
-        ideal: 20,
-        frio: 10,
-        muito_frio: 5
-    };
-
-    var classe_temperatura = 'cor-alerta';
-
-    if (temp >= limites.muito_quente) {
-        classe_temperatura = 'cor-alerta perigo-quente';
-        grauDeAviso = 'perigo quente'
-        grauDeAvisoCor = 'cor-alerta perigo-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp < limites.muito_quente && temp >= limites.quente) {
-        classe_temperatura = 'cor-alerta alerta-quente';
-        grauDeAviso = 'alerta quente'
-        grauDeAvisoCor = 'cor-alerta alerta-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp < limites.quente && temp > limites.frio) {
-        classe_temperatura = 'cor-alerta ideal';
-        removerAlerta(idAquario);
-    }
-    else if (temp <= limites.frio && temp > limites.muito_frio) {
-        classe_temperatura = 'cor-alerta alerta-frio';
-        grauDeAviso = 'alerta frio'
-        grauDeAvisoCor = 'cor-alerta alerta-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-    else if (temp <= limites.muito_frio) {
-        classe_temperatura = 'cor-alerta perigo-frio';
-        grauDeAviso = 'perigo frio'
-        grauDeAvisoCor = 'cor-alerta perigo-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
-    }
-
-    var card;
-
-    if (document.getElementById(`temp_aquario_${idAquario}`) != null) {
-        document.getElementById(`temp_aquario_${idAquario}`).innerHTML = temp + "°C";
-    }
-
-    if (document.getElementById(`card_${idAquario}`)) {
-        card = document.getElementById(`card_${idAquario}`)
-        card.className = classe_temperatura;
-    }
 }
 
-function exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor) {
-    var indice = alertas.findIndex(item => item.idAquario == idAquario);
 
-    if (indice >= 0) {
-        alertas[indice] = { idAquario, temp, grauDeAviso, grauDeAvisoCor }
-    } else {
-        alertas.push({ idAquario, temp, grauDeAviso, grauDeAvisoCor });
-    }
-
-    exibirCards();
-}
-
-function removerAlerta(idAquario) {
-    alertas = alertas.filter(item => item.idAquario != idAquario);
-    exibirCards();
-}
-
-function exibirCards() {
-    alerta.innerHTML = '';
-
-    for (var i = 0; i < alertas.length; i++) {
-        var mensagem = alertas[i];
-        alerta.innerHTML += transformarEmDiv(mensagem);
-    }
-}
-
-function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
-
-    var descricao = JSON.parse(sessionStorage.AQUARIOS).find(item => item.id == idAquario).descricao;
-    return `
-    <div class="mensagem-alarme">
-        <div class="informacao">
-            <div class="${grauDeAvisoCor}">&#12644;</div> 
-            <h3>${descricao} está em estado de ${grauDeAviso}!</h3>
-            <small>Temperatura capturada: ${temp}°C.</small>   
-        </div>
-        <div class="alarme-sino"></div>
-    </div>
-    `;
-}
 
  async function atualizacaoPeriodica() {
     var refrigeradores = JSON.parse(sessionStorage.getItem('Refrigeradores'));
     refrigeradores.forEach(item => {
         obterdados(item.idRefrigerador, item.fkEstabelecimento, item.fkCliente);
+
+       
    
   
     });
@@ -181,8 +100,6 @@ function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
 
 
 
-    })
-
-            // console.log(refrigeradores);
+    })    
     setTimeout(atualizacaoPeriodica, 5000);
 }
--- SPRINT 2/Site_Institucional/src/models/medidaModel.js
+++ SPRINT 2/Site_Institucional/src/models/medidaModel.js
@@ -18,7 +18,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
 function buscarMedidasEmTempoReal(idRefrigerador, idEstabelecimento, idCliente) {
 
     var instrucaoSql = ` 
-    select idDadoTemperatura as 'IdTemp', Temperatura, Horario from 
+    select idDadoTemperatura as 'IdTemp', dadostemperatura.fkRefrigerador as 'idRefrigerador', Temperatura, DATE_FORMAT(Horario,'%H:%i:%s') as 'Horario' from 
     dadostemperatura where dadostemperatura.fkRefrigerador = ${idRefrigerador} and dadostemperatura.fkEstabelecimento = ${idEstabelecimento} 
     and dadostemperatura.fkCliente = ${idCliente} order by idTemp desc limit 1;`
 
@@ -27,7 +27,7 @@ function buscarMedidasEmTempoReal(idRefrigerador, idEstabelecimento, idCliente) 
 }
 function buscarMedidasEmTempoRealPorta(idporta, idrefrigerador, idestabelecimento, idcliente) {
 
-    var instrucaoSql = `select idDadoAbertura as 'idAbertura', Aberturas, Horario, fkSensorBloqueio as 'Porta', dadosabertura.fkrefrigerador as 'Refrigerador',
+    var instrucaoSql = `select idDadoAbertura as 'idAbertura', Aberturas, DATE_FORMAT(Horario,'%H:%i:%s') as 'Horario', fkSensorBloqueio as 'Porta', dadosabertura.fkrefrigerador as 'Refrigerador',
     dadosabertura.fkestabelecimento as 'Estabelecimento',
     dadosabertura.fkcliente as 'cliente' from dadosabertura 
     where dadosabertura.fkporta = ${idporta} and dadosabertura.fkrefrigerador = ${idrefrigerador} and dadosabertura.fkestabelecimento = ${idestabelecimento} and dadosabertura.fkcliente = ${idcliente} order by idAbertura desc limit 1;`
